56e9fce002ff3b3cce36056879152a2e95e3c026
Adam Ligart <ligartadam@gmail.com>
2020-12-14 17:53:44 +0100

    comit2
    comit2

---
+++ ZHC/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c4cfca083abc61ab7b48e77d7c6a9f35a12682df',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4cfca083abc61ab7b48e77d7c6a9f35a12682df',
+    ),
+  ),
+);
